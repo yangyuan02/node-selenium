cdbd9dbef955993c13483b561d1b6b5562b79453
yangyuan <1367511704@qq.com>
2019-04-11 21:32:06 +0800

    fix(验证码)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const { Builder, By, Key, until, Button } = require('selenium-webdriver');
 var chrome = require('selenium-webdriver/chrome');
 var path = require('chromedriver').path;
+var tesseract = require('tesseract.js');
 
 var service = new chrome.ServiceBuilder(path).build();
 chrome.setDefaultService(service);
@@ -18,12 +19,44 @@ var windowMax = () => {
 
 var open = async () => {
   // 打开浏览器
-  return await driver.get('http://localhost:3000/#!/login');
+  return await driver.get('http://localhost:3000/#!/index');
 };
+
+var recognition = async url => {
+  console.log(url);
+  return await tesseract.recognize(url);
+};
+
 var setFromValue = () => {
   var username = driver.findElement(By.name('userName')).sendKeys('sd_shengda');
   var password = driver.findElement(By.name('passWord')).sendKeys('shengda123456');
-  driver.findElement(By.className('login-btn-box')).click(); // 点击登录
+  var inputCode = driver.findElement(By.name('inputCode'));
+  var loginBtn = driver.findElement(By.className('login-btn-box'));
+  function deep() {
+    if (!loginBtn) {
+      return;
+    }
+    if (time) {
+      clearTimeout(time);
+    }
+    var time = setTimeout(() => {
+      driver
+        .findElement(By.id('vCode'))
+        .getAttribute('src')
+        .then(data => {
+          recognition(data).then(res => {
+            console.log(res.text);
+            inputCode.clear();
+            inputCode.sendKeys(res.text);
+            loginBtn.click(); // 点击登录
+            setTimeout(() => {
+              deep();
+            }, 4000);
+          });
+        });
+    }, 1000);
+  }
+  deep();
   createImags();
 };
 var takeScreenshot = async () => {
